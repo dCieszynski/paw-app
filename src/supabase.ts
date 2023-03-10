573e0b37b6c600f456322f11c254d0e249673df9
Dawid <d.cieszynski@syberry.com>
2023-03-10 10:28:31 +0100

    Fix env variables

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,8 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  "https://ptsiwtpctamyuwnfigwp.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0c2l3dHBjdGFteXV3bmZpZ3dwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY1NjM2ODksImV4cCI6MTk5MjEzOTY4OX0.IA1bUXHCFJiZtuYkCc-W_ge8ZWuhohesfwILB-qrtMg"
-);
+function getSupabaseKey(): string {
+  return import.meta.env.VITE_SUPABASE_KEY.length === 0 || import.meta.env.VITE_SUPABASE_KEY === undefined
+    ? process.env.VITE_SUPABASE_KEY
+    : import.meta.env.VITE_SUPABASE_KEY;
+}
+
+const supabaseKey = getSupabaseKey();
+
+const supabase = createClient("https://ptsiwtpctamyuwnfigwp.supabase.co", supabaseKey);
 
 export default supabase;
