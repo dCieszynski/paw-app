b315295e8384b0e76dc9ba2e500a6f86636997a7
Dawid <d.cieszynski@syberry.com>
2023-03-10 10:28:31 +0100

    Fix env variables

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,8 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  "https://ptsiwtpctamyuwnfigwp.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0c2l3dHBjdGFteXV3bmZpZ3dwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY1NjM2ODksImV4cCI6MTk5MjEzOTY4OX0.IA1bUXHCFJiZtuYkCc-W_ge8ZWuhohesfwILB-qrtMg"
-);
+function getSupabaseKey() {
+  return import.meta.env.VITE_SUPABASE_KEY ?? process.env.REACT_APP_SUPABASE_KEY;
+}
+
+const supabase = createClient("https://ptsiwtpctamyuwnfigwp.supabase.co", getSupabaseKey());
 
 export default supabase;
